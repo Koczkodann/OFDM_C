38f48f58791a5a677e3835936caf88baf2b9d1f3
Koczkodann <p.d.turzynski@gmail.com>
2024-01-24 18:46:08 +0100

    Implementing OFDM reciver

--- src/OFDM_rcv.c
+++ src/OFDM_rcv.c
@@ -11,12 +11,15 @@ int cpIndex = 5; //wspolczynnik dlugosci cyklicznego prefiksu
 
 
 int main() {
+
+	/// LOADING TEXT FILE
     char *filename = "dataOut.txt";
     char *outname = "2dataOut.txt";
-    kiss_fft_cpx *input =(kiss_fft_cpx *)malloc((checkFileLenght(filename)) * sizeof(kiss_fft_cpx));
+    int inputLen = checkFileLenghtLine(filename);
+    kiss_fft_cpx *input =(kiss_fft_cpx *)malloc((inputLen) * sizeof(kiss_fft_cpx));
     loadFileCpx(input,filename);
 
-    int inputLen = sizeof(input) / sizeof(input[0]);
+
 
     /// Parametry dla OFDM
     	float bOFDMch = floor((float)bandWidth/(float)noSubcarriers);
@@ -25,40 +28,37 @@ int main() {
     	float guardSamples = ceil(nOFDMch/(float)cpIndex);
     	int nZeros = (nOFDMch - noSubcarriers)/2;
     	int cp = guardSamples;
-    	float packetsNum = ceil((float)inputLen/((float)noSubcarriers)+2*nZeros+cp);
+    	int packetsNum = ceil((float)inputLen/(((float)noSubcarriers) + 2*nZeros + cp))-1;
     /// Koniec parametrow
+
+    	printf("packetsNum: %d ", packetsNum);
 
     	kiss_fft_cpx **OFDMbaseband = (kiss_fft_cpx **)malloc(packetsNum*sizeof(kiss_fft_cpx*));
     	    for (int i = 0; i < packetsNum; i++){
-    	    	OFDMbaseband[i] = (float *)malloc((noSubcarriers + 2*nZeros) * sizeof(kiss_fft_cpx));
+    	    	OFDMbaseband[i] = (kiss_fft_cpx *)malloc((noSubcarriers + 2*nZeros + cp) * sizeof(kiss_fft_cpx));
     	    }
 
     	    for(int i = 0; i < packetsNum; i++){
-    	    	for(int j = 0; j < noSubcarriers; j++){
+    	    	for(int j = 0; j < noSubcarriers +2*nZeros + cp; j++){
     	    		if(j+i*noSubcarriers<inputLen){
-    	    			OFDMbaseband[i][j]=input[j+i*noSubcarriers];
+    	    			OFDMbaseband[i][j].r=input[(i*noSubcarriers+i*2*nZeros+i*cp)+j].r;
+    	    			OFDMbaseband[i][j].i=input[(i*noSubcarriers+i*2*nZeros+i*cp)+j].i;
     	    		}
     	    		else
     	    			break;
     	    	}
     	    }
 
-    	    kiss_fft_cpx **guarded = (kiss_fft_cpx **)malloc(packetsNum * sizeof(kiss_fft_cpx*));
+    	    kiss_fft_cpx **deguarded = (kiss_fft_cpx **)malloc(packetsNum * sizeof(kiss_fft_cpx*));
     	    for (int i = 0; i < packetsNum; i++) {
-    	        guarded[i] = (kiss_fft_cpx *)malloc((noSubcarriers + 2 * nZeros) * sizeof(kiss_fft_cpx));
+    	        deguarded[i] = (kiss_fft_cpx *)malloc((noSubcarriers + 2 * nZeros) * sizeof(kiss_fft_cpx));
     	    }
 
-    	    // Removing CyclePrefix //TODO
+    	    // Removing CyclePrefix
     	       for (int i = 0; i < packetsNum; i++){
-    	       	for (int j = 0; j <noSubcarriers + 2 * nZeros + cp; j++){
-    	       		if(j <= cp){
-    	       		guarded[i][j].r = OFDMbaseband[i][noSubcarriers + 2 * nZeros - cp + j].r;
-    	       		guarded[i][j].i = OFDMbaseband[i][noSubcarriers + 2 * nZeros - cp + j].i;
-    	       		}
-    	       		if(j > cp){
-    	       		guarded[i][j].r = OFDMbaseband[i][j - cp].r;
-    	       		guarded[i][j].i = OFDMbaseband[i][j - cp].i;
-    	       		}
+    	       	for (int j = 0; j <noSubcarriers + 2 * nZeros; j++){
+    	       		deguarded[i][j].r = OFDMbaseband[i][cp + j].r;
+    	       		deguarded[i][j].i = OFDMbaseband[i][cp + j].i;
     	       	}
     	       }
 
@@ -76,27 +76,68 @@ int main() {
     	       // FFT
     	       for (int i = 0; i < packetsNum; i++) {
     	           for (int j = 0; j < noSubcarriers + 2 * nZeros; j++) {
-    	               fftInput[i][j].r = OFDMbaseband[i][j].r;
-    	               fftInput[i][j].i = OFDMbaseband[i][j].i;
+    	               fftInput[i][j].r = deguarded[i][j].r;
+    	               fftInput[i][j].i = deguarded[i][j].i;
     	           }
     	           kiss_fft(cfg_inv, fftInput[i], fftOutput[i]);
     	       }
 
+    	       kiss_fft_cpx **trainBuffer = (kiss_fft_cpx **)malloc(packetsNum * sizeof(kiss_fft_cpx *));
+    	       for (int i = 0; i < packetsNum; i++) {
+    	           	           trainBuffer[i] = (kiss_fft_cpx *)malloc((noSubcarriers) * sizeof(kiss_fft_cpx));
+    	           	       }
+
+
     	       fftShift(fftOutput, packetsNum, noSubcarriers + 2*nZeros);
-    	       rmZeros(fftOutput, packetsNum, noSubcarriers, nZeros);
+    	       rmZeros(fftOutput, trainBuffer, packetsNum, noSubcarriers, nZeros);
 
-    	       printf("Nzeros: %d  ", nZeros);
 
+    	       float *trainSequence = (float *)malloc(packetsNum*noSubcarriers*sizeof(float *));
+
+    	       for(int i = 0; i < packetsNum; i++){
+    	       	for(int j = 0; j < noSubcarriers; j++){
+    	       			trainSequence[j+i*noSubcarriers] = trainBuffer[i][j].r;
+    	       	}
+    	       }
+
+    	       float *output = (float *)malloc(packetsNum*noSubcarriers*sizeof(float *));
+    	       bpskDemodulation(1, trainSequence, output, noSubcarriers*packetsNum);
+
+    	       ///DEBUG
     	       for(int i = 0; i < packetsNum; i++)
     	       {
-    	          	for(int j = 0; j < noSubcarriers + 2*nZeros + cp; j++)
+    	          	for(int j = 0; j < noSubcarriers; j++)
     	          	{
     	          		printf("|%d - %d: ", i, j);
-    	          		printf("%f ", fftOutput[i][j].r);
+    	          		printf("%f ", output[i*noSubcarriers + j]);
     	          	}
     	       }
 
-    	       //bpskdemodulation(1, fftOutput, outData, )
+    	       saveFileBin(output, outname, packetsNum, noSubcarriers);
+
+    	       /// FREE ALOCATED MEMORY
+    	       free(input);
+    	       for (int i = 0; i < packetsNum; i++) {
+    	           free(OFDMbaseband[i]);
+    	       }
+    	       free(OFDMbaseband);
+    	       for (int i = 0; i < packetsNum; i++) {
+    	           free(deguarded[i]);
+    	       }
+    	       free(deguarded);
+    	       for (int i = 0; i < packetsNum; i++) {
+    	           free(fftInput[i]);
+    	           free(fftOutput[i]);
+    	       }
+    	       free(fftInput);
+    	       free(fftOutput);
+    	       kiss_fft_free(cfg_inv);
+    	       for (int i = 0; i < packetsNum; i++) {
+    	           free(trainBuffer[i]);
+    	       }
+    	       free(trainBuffer);
+    	       free(trainSequence);
+    	       free(output);
 
 
 	return 0;
